3d933631f9eb04b966c7538658182fc235f7a594
denizbabig <denizbabig@gmail.com>
2025-10-07 12:05:54 +0200

    First Commit - starting out

---
+++ src/test/java/htw/webtech/project_app/ProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.project_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
